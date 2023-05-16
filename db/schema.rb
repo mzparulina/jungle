0eb8c510062fa337d8e90211cbc5e688c02d9356
mzparulina <pauline.revilla@gmail.com>
2023-05-15 23:13:22 -0400

    Migrate user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_062916) do
+ActiveRecord::Schema.define(version: 2023_05_16_025836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", id: :serial, force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "user_id"
+    t.integer "quantity", default: 0, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -31,6 +37,19 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
+  create_table "menu_items", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.text "description"
+    t.money "price", scale: 2, default: "0.0", null: false
+    t.boolean "availability", default: true, null: false
+    t.string "image_url", limit: 255
+  end
+
+  create_table "order_items", id: :serial, force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "menu_items_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -53,7 +72,22 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "status", id: :serial, force: :cascade do |t|
+    t.string "status", limit: 255, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "cart_items", "menu_items", column: "item_id", name: "cart_items_item_id_fkey", on_delete: :cascade
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
+  add_foreign_key "order_items", "menu_items", column: "menu_items_id", name: "order_items_menu_items_id_fkey", on_delete: :cascade
   add_foreign_key "products", "categories"
 end
