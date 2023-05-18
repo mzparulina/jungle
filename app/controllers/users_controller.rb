e00ee0fd36ca76043246d10645a6b24529e7cb41
mzparulina <pauline.revilla@gmail.com>
2023-05-18 07:39:26 -0400

    Add controllers for login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
+  def create
+    @user = User.new(user_params)
+
+    # Save session in server, the redirect to homepage
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      redirect_to [:new, :user]
+    end
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
